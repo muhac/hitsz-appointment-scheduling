233caf1de99557113e6badd40d36250af2932a33
Muhan Li <limuhan@msn.com>
2021-03-05 04:05:20 +0800

    更新预约及查询

--- backend/new.py
+++ backend/new.py
@@ -8,14 +8,15 @@ with open('data/available.json', 'w') as f:
     json.dump({}, f)
 
 with open('data/in_progress.json', 'w') as f:
-    json.dump({}, f)
+    json.dump([], f)
 
 info = {
     'teachers': ['应梦娴', '田薇', '梁羡飞', '李娜'],
     'time_format': '%Y 年 %m 月 %d 日  （%A）',
     'work_start': [9, 10, 14, 15, 16],
     'work_hours': '{0:02d}:00 ~ {0:02d}:50',
-    'max_capacity': 1
+    'max_capacity': 1,
+    'sort_helper': '%Y 年 %m 月 %d 日  （%A）%M:00 ~ %S:50'
 }
 
 with open('data/settings.json', 'w') as f:
