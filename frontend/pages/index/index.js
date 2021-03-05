8b48818d03d0b65c2eae9fbb8a94d908c2002809
Muhan Li <github@umm.one>
2021-03-05 19:23:29 +0800

    格式化文档

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -4,12 +4,12 @@ const app = getApp()
 
 Page({
   data: {
-    mask_show:true,
+    mask_show: true,
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    code:[],
+    code: [],
   },
   // 事件处理函数
   bindViewTap() {
@@ -27,30 +27,27 @@ Page({
       url: '../book2/book2'
     })
   },
-  onLoad:function() {
-    var that=this
-    console.log("hi")
+  onLoad: function () {
+    var that = this
     //登录
     wx.login({
-      success(res){
+      success(res) {
         wx.request({
           url: 'https://www.bugstop.site/uid/',
           headers: {
             'Content-Type': 'application/json'
-        },
-          data:res,
-          method:"POST",
-          timeout:10000,
-          success(res){
+          },
+          data: res,
+          method: "POST",
+          timeout: 10000,
+          success(res) {
             console.log(res.data)
           }
         })
       },
-      fail(){
+      fail() {
         console.log("fail")
       }
     })
   },
-
-
 })
