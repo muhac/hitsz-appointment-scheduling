fe656fb572e0542098f8fabf21adff51f7393bf7
Muhan Li <github@umm.one>
2021-03-05 23:57:32 +0800

    嘻嘻

--- frontend/pages/admin_verify/admin_verify.js
+++ frontend/pages/admin_verify/admin_verify.js
@@ -1,66 +1,44 @@
-// pages/admin_verify/admin_verify.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    Length: 6,        //输入框个数
+    isFocus: true,    //聚焦
+    Value: "",        //输入的内容
+    ispassword: true, //是否密文显示 true为密文， false为明文。
+    disabled:true,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  Focus(e) {
+    var that = this;
+    console.log(e.detail.value);
+    var inputValue = e.detail.value;
+    var ilen = inputValue.length;
+    if(ilen == 6){
+      that.setData({
+        disabled: false,
+      })
+    }else{
+      that.setData({
+        disabled: true,
+      })
+    }
+    that.setData({
+      Value: inputValue,
+    })
+  },
+  Tap() {
+    var that = this;
+    that.setData({
+      isFocus: true,
+    })
+  },
+  formSubmit(e) {
+    console.log(e.detail.value.password);
+  },
+ 
   onLoad: function (options) {
-
+  
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
+  
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
