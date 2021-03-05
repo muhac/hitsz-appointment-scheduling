f1b944584be6d7cab591e2bd4c0a7c685c53fa1b
Muhan Li <github@umm.one>
2021-03-05 16:21:30 +0800

    更改预约列表样式

--- backend/new.py
+++ backend/new.py
@@ -1,5 +1,6 @@
 import os
 import json
+import hashlib
 
 if not os.path.isdir('data'):
     os.mkdir('data')
@@ -8,15 +9,17 @@ with open('data/available.json', 'w') as f:
     json.dump({}, f)
 
 with open('data/in_progress.json', 'w') as f:
-    json.dump([], f)
+    json.dump({}, f)
 
 info = {
+    'password': hashlib.md5("password".encode('utf-8')).hexdigest(),
     'teachers': ['应梦娴', '田薇', '梁羡飞', '李娜'],
-    'time_format': '%Y 年 %m 月 %d 日  （%A）',
+    'time_format': '%Y 年 %m 月 %d 日 ・ %A',
     'work_start': [9, 10, 14, 15, 16],
     'work_hours': '{0:02d}:00 ~ {0:02d}:50',
     'max_capacity': 1,
-    'sort_helper': '%Y 年 %m 月 %d 日  （%A）%M:00 ~ %S:50'
+    'max_days': 10,
+    'sort_helper': '%Y 年 %m 月 %d 日 ・ %A%M:00 ~ %S:50'
 }
 
 with open('data/settings.json', 'w') as f:
