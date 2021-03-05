1f1b894cc7b4b773605ce1fd7afca06c742bb8eb
Muhan Li <limuhan@msn.com>
2021-03-05 19:11:23 +0800

    工作日

--- backend/new.py
+++ backend/new.py
@@ -11,16 +11,19 @@ with open('data/available.json', 'w') as f:
 with open('data/in_progress.json', 'w') as f:
     json.dump({}, f)
 
-info = {
-    'password': hashlib.md5("password".encode('utf-8')).hexdigest(),
+settings = {
+    'password': hashlib.md5("000000".encode('utf-8')).hexdigest(),
     'teachers': ['应梦娴', '田薇', '梁羡飞', '李娜'],
     'time_format': '%Y 年 %m 月 %d 日 ・ %A',
-    'work_start': [9, 10, 14, 15, 16],
-    'work_hours': '{0:02d}:00 ~ {0:02d}:50',
+    'work_start': [9, 10, 11, 14, 15, 16],
+    'work_hours': '{:02d}:00 ~ {:02d}:30',
+    'work_days': ['星期一', '星期二', '星期三', '星期四', '星期五'],
     'max_capacity': 1,
     'max_days': 10,
+    'ticket_format': '{:05d}@{}',
+    'timestamp': '%Y 年 %m 月 %d 日 %H 时 %M 分 %S 秒',
     'sort_helper': '%Y 年 %m 月 %d 日 ・ %A%M:00 ~ %S:50'
 }
 
 with open('data/settings.json', 'w') as f:
-    json.dump(info, f)
+    json.dump(settings, f)
--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,7 @@
 import json
+import requests
 from datetime import datetime, timedelta
+
 from flask import *
 from flask_cors import CORS
 
@@ -17,30 +19,38 @@ def date_lang(date: str, lang: (str, str) = ('en', 'zh')) -> str:
     return date
 
 
+def construct_response(msg):
+    response = make_response(jsonify(msg))
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,HEAD,GET,POST'
+    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
+    return response
+
+
 app = Flask(__name__, )
 CORS(app, resources=r'/*')
 
 
 @app.route("/", methods=['GET'])
 def index():
-    messages = {"statusCode": 200, 'GitHub': 'bugstop'}
-
-    response = make_response(jsonify(messages))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,HEAD,GET,POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
-    return response
+    messages = {"statusCode": 200, 'GitHub': 'bugstop', 'copyright': 2021}
+    return construct_response(messages)
 
 
-@app.route("/verify/", methods=['GET'])
+@app.route("/uid/", methods=['POST'])
+def get_uid():
+    code = request.json.get('code')
+    url = 'https://api.weixin.qq.com/sns/jscode2session?appid={}&secret={}&js_code={}&grant_type=authorization_code'
+    rc = requests.get(url.format('wxdefe17992df5e3fb', '7380e978aeec60c86fd23e97e184f250', code))
+    print(rc.json())
+    messages = {"statusCode": 200, 'wx': rc.json().get('openid')}
+    return construct_response(messages)
+
+
+@app.route("/verify/", methods=['POST'])
 def admin_verification():
-    messages = {"statusCode": 200, 'password': settings['password']}
-
-    response = make_response(jsonify(messages))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,HEAD,GET,POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
-    return response
+    messages = {"statusCode": 200 if settings['password'] == request.json.get('password') else 500}
+    return construct_response(messages)
 
 
 @app.route("/reserve/", methods=['POST'])
@@ -49,16 +59,9 @@ def reserve():
         with open('data/available.json') as f:
             schedule = json.load(f)
 
-        if not all([
-            data.get('name'),
-            data.get('sex'),
-            data.get('id'),
-            data.get('mobile'),
-            data.get('teacher'),
-            data.get('date'),
-            data.get('hour'),
-            data.get('detail'),
-        ]) or schedule[data['date']][data['hour']] < 1:
+        if not all([data.get('wx'), data.get('name'), data.get('sex'), data.get('id'), data.get('mobile'),
+                    data.get('teacher'), data.get('date'), data.get('hour'), data.get('detail')]) \
+                or schedule[data['date']][data['hour']] < 1:
             print('check failed')
             raise RuntimeError
 
@@ -72,7 +75,9 @@ def reserve():
 
         tickets = sorted(list(appointments.keys()), key=lambda z: int(z.split('@')[0]))
         ticket_id = 1 if not tickets else int(tickets[-1].split('@')[0]) + 1
-        new_ticket = '{:05d}@{}'.format(ticket_id, datetime.now().strftime('%Y%m%d%M%S'))
+        new_ticket = settings['ticket_format'].format(ticket_id, datetime.now().strftime('%Y%m%d%M%S'))
+
+        data['timestamp'] = datetime.now().strftime(settings['timestamp'])
         appointments[new_ticket] = data
 
         with open('data/in_progress.json', 'w') as f:
@@ -90,43 +95,37 @@ def reserve():
         print(e)
         messages['statusCode'] = 500
 
-    response = make_response(jsonify(messages))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,HEAD,GET,POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
-    return response
+    return construct_response(messages)
 
 
-@app.route("/list/", methods=['GET'])
+@app.route("/list/", methods=['POST'])
 def in_progress():
-    def get_data():
+    def get_data(user_filter: str = None):
         with open('data/in_progress.json') as f:
             appointments = json.load(f)
 
-        tickets = sorted(list(appointments.keys()),
-                         key=lambda z: datetime.strptime(
-                             date_lang(appointments[z]['date'] +
-                                       appointments[z]['hour'], ('zh', 'en')),
-                             settings['sort_helper'])
-                         )
+        tickets = sorted(list(appointments.keys()), key=lambda z: datetime.strptime(
+            date_lang(appointments[z]['date'] + appointments[z]['hour'], ('zh', 'en')), settings['sort_helper']))
+
+        if user_filter != settings['password']:
+            tickets_filtered = [ticket for ticket in tickets if appointments[ticket].get('wx') == user_filter]
+            appointments_filtered = {ticket: appointments[ticket] for ticket in tickets_filtered}
+            appointments, tickets = appointments_filtered, tickets_filtered
 
         return appointments, tickets
 
     messages = {'statusCode': 200}
 
     try:
-        data = get_data()
+        username = request.json.get('user')
+        data = get_data(username)
         messages['tickets'] = data[1]
         messages['inProgress'] = data[0]
     except Exception as e:
         print(e)
         messages['statusCode'] = 500
 
-    response = make_response(jsonify(messages))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,HEAD,GET,POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
-    return response
+    return construct_response(messages)
 
 
 @app.route("/available/", methods=['GET'])
@@ -139,8 +138,10 @@ def available():
         dates.sort(key=lambda z: datetime.strptime(z, settings['time_format']))
 
         if not dates or len(dates) != max_days or \
-                datetime.strptime(dates[0], settings['time_format']) < datetime.now() - timedelta(days=1) or \
-                datetime.strptime(dates[-1], settings['time_format']) < datetime.now() + timedelta(days=max_days - 2):
+                datetime.strptime(dates[0], settings['time_format']) < \
+                datetime.now() - timedelta(days=1) or \
+                datetime.strptime(dates[-1], settings['time_format']) < \
+                datetime.now() + timedelta(days=max_days - 2):
             print('update schedule')
 
             schedule_new = {}
@@ -148,7 +149,7 @@ def available():
                 d = date_lang((datetime.now() + timedelta(days=day)).strftime(settings['time_format']))
                 schedule_new[d] = {}
                 for hour in settings['work_start']:
-                    h = settings['work_hours'].format(hour)
+                    h = settings['work_hours'].format(hour, hour)
 
                     if d in dates and h in list(schedule[d].keys()):
                         schedule_new[d][h] = schedule[d][h]
@@ -159,9 +160,12 @@ def available():
                 json.dump(schedule_new, f)
             schedule = schedule_new
 
-        date = sorted(list(schedule.keys()),
-                      key=lambda z: datetime.strptime(date_lang(z, ('zh', 'en')), settings['time_format']))
+        date = sorted(list(schedule.keys()), key=lambda z: datetime.strptime(
+            date_lang(z, ('zh', 'en')), settings['time_format']))
         hour = sorted(list(schedule[list(schedule.keys())[0]].keys()), key=lambda z: int(z[:2]))
+
+        date = [d for d in date if any(work_day in d for work_day in settings['work_days'])]
+        schedule = {d: schedule[d] for d in date}
 
         return schedule, date, hour
 
@@ -173,15 +177,12 @@ def available():
         messages['hour'] = data[2]
         messages['schedule'] = data[0]
         messages['teachers'] = settings['teachers']
+        print(messages['date'])
     except Exception as e:
         print(e)
         messages['statusCode'] = 500
 
-    response = make_response(jsonify(messages))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,HEAD,GET,POST'
-    response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
-    return response
+    return construct_response(messages)
 
 
 if __name__ == '__main__':
