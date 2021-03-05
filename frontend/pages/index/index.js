d71f434077678df8d7254b58924e918c72cc8e5a
youjunhua@stu.hit.edu.cn <barryyou99@gmail.com>
2021-03-05 18:15:02 +0800

    增加login

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -4,10 +4,12 @@ const app = getApp()
 
 Page({
   data: {
+    mask_show:true,
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    code:[],
   },
   // 事件处理函数
   bindViewTap() {
@@ -25,40 +27,30 @@ Page({
       url: '../book2/book2'
     })
   },
-  onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+  onLoad:function() {
+    var that=this
+    console.log("hi")
+    //登录
+    wx.login({
+      success(res){
+        wx.request({
+          url: 'https://www.bugstop.site/uid/',
+          headers: {
+            'Content-Type': 'application/json'
+        },
+          data:res,
+          method:"POST",
+          timeout:10000,
+          success(res){
+            console.log(res.data)
+          }
         })
+      },
+      fail(){
+        console.log("fail")
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
-  }
+  },
+
+
 })
