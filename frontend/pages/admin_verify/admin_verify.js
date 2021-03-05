86db9033d1109c39b0f000cf97efc4cc40672d54
Muhan Li <limuhan@msn.com>
2021-03-06 02:07:46 +0800

    格式化

--- frontend/pages/admin_verify/admin_verify.js
+++ frontend/pages/admin_verify/admin_verify.js
@@ -1,22 +1,21 @@
-
 Page({
   data: {
-    Length: 6,        //输入框个数
-    isFocus: true,    //聚焦
-    Value: "",        //输入的内容
+    Length: 6, //输入框个数
+    isFocus: true, //聚焦
+    Value: "", //输入的内容
     ispassword: true, //是否密文显示 true为密文， false为明文。
-    disabled:true,
+    disabled: true,
   },
   Focus(e) {
     var that = this;
     console.log(e.detail.value);
     var inputValue = e.detail.value;
     var ilen = inputValue.length;
-    if(ilen == 6){
+    if (ilen == 6) {
       that.setData({
         disabled: false,
       })
-    }else{
+    } else {
       that.setData({
         disabled: true,
       })
@@ -34,11 +33,11 @@ Page({
   formSubmit(e) {
     console.log(e.detail.value.password);
   },
- 
+
   onLoad: function (options) {
-  
+
   },
   onShow: function () {
-  
+
   },
 })
