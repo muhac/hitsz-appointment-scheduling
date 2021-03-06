54ce5fbd6602b7cdc11056b4538ace57452b4e12
Muhan Li <limuhan@msn.com>
2021-03-06 18:39:09 +0800

    update

--- frontend/app.js
+++ frontend/app.js
@@ -1,15 +1,52 @@
 // app.js
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+    var that=this
+    wx.showLoading({
+      title: "登陆中",
+      mask: true,
+  });
     // 登录
     wx.login({
+      
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+
+        
+        wx.request({
+          url: 'https://www.bugstop.site/uid/',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: res,
+          method: "POST",
+          timeout: 10000,
+          success(res) {
+            console.log(res.data)
+            if(res.statusCode==200){
+              wx.hideLoading();
+              that.globalData.wx = res.data.wx
+            }
+            else{
+              wx.hideLoading();
+              wx.showToast({
+                title: "登录失败",
+                icon: 'error', //如果要纯文本，不要icon，将值设为'none'
+                mask: true,
+                duration: 3000
+            })
+            }
+          },
+          fail(){
+            wx.hideLoading();
+            wx.showToast({
+              title: "连接超时",
+              icon: 'error', //如果要纯文本，不要icon，将值设为'none'
+              mask: true,
+              duration: 3000
+          })
+          }
+        })
       }
     })
     // 获取用户信息
@@ -34,6 +71,8 @@ App({
     })
   },
   globalData: {
-    userInfo: ''
+    userInfo: null,
+    wx:null,
+    admin_password:null
   }
 })
