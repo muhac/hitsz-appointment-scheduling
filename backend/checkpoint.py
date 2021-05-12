2ef3f9a2839120234e5df582c327414373538b39
Muhan Li <limuhan@msn.com>
2021-05-12 19:41:29 +0800

    定时自动保存

---
+++ backend/checkpoint.py
@@ -0,0 +1,8 @@
+import requests
+
+sess = requests.session()
+sess.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 '
+                                   '(KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36'})
+response = sess.get('https://www.bugstop.site/')
+
+print(response.json())
